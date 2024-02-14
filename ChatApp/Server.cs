a5449de098350358523abfeb61f6c373097ddd2c
Alexander <a.zhukov1995@gmail.com>
2024-02-14 17:33:07 +0400

    chat v1

---
+++ ChatApp/Server.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualBasic;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ChatServerApp
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var server = new Server();
+            await server.Run();
+        }
+    }
+
+    public class Server
+    {
+        TcpListener listener = new TcpListener(IPAddress.Any, 8888);
+        List<TcpClient> users = new List<TcpClient>();
+
+        public async Task Run()
+        {
+            try
+            {
+                listener.Start();
+
+                await Console.Out.WriteLineAsync("Сервер запущен!");
+
+                while (true)
+                {
+                    var tcpClient = await listener.AcceptTcpClientAsync();
+                    users.Add(tcpClient);
+
+                    Console.WriteLine("Пользователь успешно подключен!");
+
+                    await Task.Run(() => ProcessClient(tcpClient));
+                }
+            }
+            catch { }
+            finally { }
+        }
+
+        public async Task ProcessClient(TcpClient client)
+        {
+            var reader = new StreamReader(client.GetStream());
+
+            while (client.Connected)
+            {
+                string? msg = await reader.ReadLineAsync();
+
+                if (msg != null)
+                {
+                    foreach (var user in users)
+                    {
+                        if (user != client)
+                        {
+                            using (var writer = new StreamWriter(user.GetStream()))
+                            {
+                                await writer.WriteLineAsync(msg);
+                            };
+                        }
+                    }
+                }
+
+                reader.Close();
+            }
+        }
+    }
+}
---
+++ ChatClientApp/Client.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualBasic;
+using System.Net;
+using System.Net.Sockets;
+
+public class Client
+{
+    static async Task Main(string[] args)
+    {
+        TcpClient tcpClient = new TcpClient();
+
+        Thread.Sleep(2000);
+
+        tcpClient.Connect(IPAddress.Parse("127.0.0.1"), 8888);
+
+        Console.WriteLine("Вы успешно подключены!");
+        Console.WriteLine("Вы можете отправлять сообщения на сервер. Для выхода из чата нажмите \"q\"");
+
+        string? outMsg = string.Empty;
+        var sWriter = new StreamWriter(tcpClient.GetStream());
+        var sReader = new StreamReader(tcpClient.GetStream());
+
+        while (true)
+        {
+            Console.Write("Введите сообщение: ");
+            await Task.Run(() => outMsg = Console.ReadLine());
+
+            if (outMsg == "q") break;
+            else if (outMsg != null)
+            {
+                await sWriter.WriteLineAsync(outMsg);
+                sWriter.Flush();
+                Console.WriteLine("Ваше сообщение отправлено!");
+            }
+
+            string? inMsg = await sReader.ReadLineAsync();
+
+            if (inMsg != null) Console.WriteLine("Сторонний пользователь написал: " + inMsg);
+
+        }
+        Console.WriteLine("Приложение закрывается");
+        Console.ReadLine();
+
+    }
+}
